4adfea065e11fe71aab49d5bbdffd7b5b572f7fe
kais skander <kaisska1998@gmail.com>
2020-05-17 18:35:56 +0200

    fixed minicards

--- src/components/dashboard/dashboardLayout/MiniCards.vue
+++ src/components/dashboard/dashboardLayout/MiniCards.vue
@@ -9,69 +9,93 @@
             <div class="row">
               <div class="col-xl-3 col-sm-6 p-2">
                 <div class="card card-common">
-                  <div class="card-body">
-                    <div class="d-flex justify-content-between">
-                      <i class="fas fa-shopping-cart fa-3x text-warning"></i>
-                      <div class="text-right text-secondary">
-                        <h5>Sales</h5>
-                        <h3>$135,000</h3>
+                  <div class="card-header">
+                    <div class="row">
+                      <div class="col">
+                      <div class="text-left text-capitalize font-weight-bold text-secondary">Ventes</div>
+                      </div>
+                      <div class="col">
+                      <span class="badge badge-primary mt-1 float-right">Mensuelle</span>
                       </div>
                     </div>
                   </div>
-                  <div class="card-footer text-secondary">
-                    <i class="fas fa-sync mr-3"></i>
-                    <span>Update Now</span>
+                  <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                      <div>
+                        <h3 class="text-secondary">40 886,200</h3>
+                        <small class="text-secondary">Total income</small>
+                        <small class="text-primary text-left flex-row-reverse ml-2">80%<i class="fas fa-level-up-alt text-primary"></i></small>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
               <div class="col-xl-3 col-sm-6 p-2">
                 <div class="card card-common">
-                  <div class="card-body">
-                    <div class="d-flex justify-content-between">
-                      <i class="fas fa-money-bill-alt fa-3x text-success"></i>
-                      <div class="text-right text-secondary">
-                        <h5>Expenses</h5>
-                        <h3>$39,000</h3>
+                  <div class="card-header">
+                    <div class="row">
+                      <div class="col">
+                        <div class="text-left text-capitalize font-weight-bold text-secondary">Depense</div>
+                      </div>
+                      <div class="col">
+                        <span class="badge badge-danger mt-1 float-right">Mensuelle</span>
                       </div>
                     </div>
                   </div>
-                  <div class="card-footer text-secondary">
-                    <i class="fas fa-sync mr-3"></i>
-                    <span>Update Now</span>
+                  <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                      <div>
+                        <h3 class="text-secondary">275,800</h3>
+                        <small class="text-secondary">Total Depense</small>
+                        <small class="text-danger text-left flex-row-reverse ml-2">20%<i class="fas fa-level-up-alt text-danger"></i></small>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
               <div class="col-xl-3 col-sm-6 p-2">
                 <div class="card card-common">
-                  <div class="card-body">
-                    <div class="d-flex justify-content-between">
-                      <i class="fas fa-users fa-3x text-info"></i>
-                      <div class="text-right text-secondary">
-                        <h5>User</h5>
-                        <h3>15,000</h3>
+                  <div class="card-header">
+                    <div class="row">
+                      <div class="col">
+                        <div class="text-left text-capitalize font-weight-bold text-secondary">projets</div>
+                      </div>
+                      <div class="col">
+                        <span class="badge badge-warning mt-1 text-capitalize float-right">annuel</span>
                       </div>
                     </div>
                   </div>
-                  <div class="card-footer text-secondary">
-                    <i class="fas fa-sync mr-3"></i>
-                    <span>Update Now</span>
+                  <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                      <div>
+                        <h3 class="text-secondary">31</h3>
+                        <small class="text-secondary text-capitalize">total projet</small>
+                        <small class="text-warning text-left flex-row-reverse ml-2">20%<i class="fa fa-bolt text-warning"></i></small>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
               <div class="col-xl-3 col-sm-6 p-2">
                 <div class="card card-common">
-                  <div class="card-body">
-                    <div class="d-flex justify-content-between">
-                      <i class="fas fa-chart-line fa-3x text-danger"></i>
-                      <div class="text-right text-secondary">
-                        <h5>Visiters</h5>
-                        <h3>45,000</h3>
+                  <div class="card-header">
+                    <div class="row">
+                      <div class="col">
+                        <div class="text-left text-capitalize font-weight-bold text-secondary">Activités</div>
+                      </div>
+                      <div class="col">
+                        <span class="badge badge-info mt-1 text-capitalize float-right">Mensuelle</span>
                       </div>
                     </div>
                   </div>
-                  <div class="card-footer text-secondary">
-                    <i class="fas fa-sync mr-3"></i>
-                    <span>Update Now</span>
+                  <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                      <div>
+                        <h3 class="text-secondary">31</h3>
+                        <small class="text-secondary text-capitalize">total activité</small>
+                        <small class="text-info text-left flex-row-reverse ml-2">60%<i class="fas fa-level-down-alt text-info"></i></small>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
