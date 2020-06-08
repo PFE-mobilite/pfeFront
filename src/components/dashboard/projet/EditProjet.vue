f1a87e8e21636a97ed049ad65b8640633c1ec095
kais skander <kaisska1998@gmail.com>
2020-06-09 00:47:15 +0200

    linking v4

--- src/components/dashboard/projet/EditProjet.vue
+++ src/components/dashboard/projet/EditProjet.vue
@@ -10,41 +10,41 @@
             <div class="row">
               <div class="col-md-6 pr-md-1">
                 <label for="">libelle</label>
-                <input type="text" class="form-control" placeholder="libelle" >
+                <input type="text" class="form-control" placeholder="libelle" v-model="projet.libelle" >
               </div>
               <div class="col-md-6 pl-md-1">
                 <label for="">Type Projet</label>
-                <input type="text" class="form-control" placeholder="type projet" >
+                <input type="text" class="form-control" placeholder="type projet" v-model="projet.typeProjet">
               </div>
             </div>
             <div class="row">
               <div class="col-md-6 pr-md-1">
                 <label for="">Client</label>
-                <input type="text" class="form-control" placeholder="libelle" >
+                <input type="email" class="form-control" placeholder="libelle" v-model="projet.contact.email">
               </div>
               <div class="col-md-6 pl-md-1">
                 <label for="">Pays</label>
-                <input type="text" class="form-control" placeholder="Pays" >
+                <input type="text" class="form-control" placeholder="Pays" v-model="projet.pays">
               </div>
             </div>
             <div class="row">
               <div class="col-md-6 pr-md-1">
                 <label for="">Devise</label>
-                <input type="text" class="form-control" placeholder="libelle" >
+                <input type="text" class="form-control" placeholder="libelle" v-model="projet.devise">
               </div>
               <div class="col-md-6 pl-md-1">
                 <label for="">Cout Estimé</label>
-                <input type="text" class="form-control" placeholder="Cout Estime" >
+                <input type="text" class="form-control" placeholder="Cout Estime" v-model="projet.coutEstime" >
               </div>
             </div>
             <div class="row">
               <div class="col-md-6 pr-md-1">
                 <label for="">Date debut</label>
-                <input type="date" class="form-control" placeholder="Date debut" >
+                <input type="date" class="form-control" placeholder="Date debut" v-model="projet.dateDebut">
               </div>
               <div class="col-md-6 px-md-1">
                 <label for="">Date fin</label>
-                <input type="date" class="form-control" placeholder="Date fin">
+                <input type="date" class="form-control" placeholder="Date fin" v-model="projet.dateFin">
               </div>
             </div>
             <div class="row">
@@ -52,7 +52,7 @@
               <div class="col-md-4 pr-md-1"></div>
               <div class="col-md-5">
                 <label for="">Cout en cours</label>
-                <input type="text" class="form-control" placeholder="1000$" disabled >
+                <input type="text" class="form-control text-dark" placeholder="250$" disabled v-model="projet.cout">
               </div>
             </div>
             <div class="row d-flex flex-row-reverse">
@@ -64,7 +64,7 @@
         </div>
       </div>
     </div>
-    <div class="row mx-5">
+    <div class="row mx-4">
       <div class="col-5 mx-5 mb-5">
         <div class="card card-list-employe">
           <div class="card-header text-capitalize font-weight-bold text-info">
@@ -76,17 +76,19 @@
                 <thead>
                 <tr class="text-muted">
                   <th>Employe</th>
-                  <th>Technologie</th>
+                  <th>technologie</th>
                   <th></th>
                 </tr>
                 </thead>
                 <tbody>
-                <td>employe</td>
-                <td>Symfony</td>
-                <td>
-                  <a href=""><i class="fas fa-edit text-success mr-2"></i></a>
-                  <a href=""><i class="fas fa-trash-alt text-danger"></i></a>
-                </td>
+                <tr v-for="(emptec,index) in technos" :key="emptec + index">
+                  <td>{{emptec.employe.email ? emptec.employe.email : 'null' }}</td>
+                  <td>{{emptec.nom}}</td>
+                  <td>
+                    <a href=""><i class="fas fa-edit text-success mr-2"></i></a>
+                    <a href=""><i class="fas fa-trash-alt text-danger"></i></a>
+                  </td>
+                </tr>
                 </tbody>
               </table>
             </div>
@@ -105,17 +107,19 @@
                 <thead>
                 <tr class="text-muted">
                   <th>Service</th>
-                  <th>Fournisseur</th>
+                  <th>Prix</th>
                   <th></th>
                 </tr>
                 </thead>
                 <tbody>
-                <td>employe</td>
-                <td>Symfony</td>
-                <td>
-                  <a href=""><i class="fas fa-edit text-success mr-2"></i></a>
-                  <a href=""><i class="fas fa-trash-alt text-danger"></i></a>
-                </td>
+                <tr v-for="(servh,index) in this.projet.serviceH" :key="servh + index">
+                  <td>{{servh.typeService}}</td>
+                  <td>{{servh.prix}}$</td>
+                  <td>
+                    <a :href="'/edit-service-internet/' + servh.id"><i class="fas fa-edit text-success mr-2"></i></a>
+                    <a href=""><i class="fas fa-trash-alt text-danger"></i></a>
+                  </td>
+                </tr>
                 </tbody>
               </table>
             </div>
@@ -125,24 +129,16 @@
                 <thead>
                 <tr class="text-muted">
                   <th>Materiel</th>
-                  <th>Fournisseur</th>
+                  <th>Prix</th>
                   <th></th>
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
-                  <td>employe</td>
-                  <td>Symfony</td>
+                <tr v-for="(mat,index) in this.projet.materiel" :key="mat + index">
+                  <td>{{mat.typeMateriel}}</td>
+                  <td>{{mat.prixAchat}}$</td>
                   <td>
-                    <a href=""><i class="fas fa-edit text-success mr-2"></i></a>
-                    <a href=""><i class="fas fa-trash-alt text-danger"></i></a>
-                  </td>
-                </tr>
-                <tr>
-                  <td>employe</td>
-                  <td>Symfony</td>
-                  <td>
-                    <a href=""><i class="fas fa-edit text-success mr-2"></i></a>
+                    <a :href="'/edit-materiel/' + mat.id"><i class="fas fa-edit text-success mr-2"></i></a>
                     <a href=""><i class="fas fa-trash-alt text-danger"></i></a>
                   </td>
                 </tr>
@@ -157,11 +153,60 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data () {
     return {
-      Projets: []
+      projet: {
+        id: '',
+        libelle: '',
+        dateDebut: '',
+        dateFin: '',
+        cout: '',
+        typeProjet: '',
+        maintenance: '',
+        devise: '',
+        pays: '',
+        coutEstime: '',
+        serviceH: [],
+        materiel: [],
+        employe: [],
+        technologie: [],
+        contact: ''
+      },
+      id: this.$route.params.id,
+      technos: []
     }
+  },
+  created () {
+    axios.get('http://127.0.0.1:8000/api/projets/' + this.id).then(res => {
+      const dataImported = res.data
+      this.projet.id = dataImported.id
+      this.projet.libelle = dataImported.libelle
+      this.projet.dateDebut = dataImported.dateDebut.substring(0, 10)
+      this.projet.dateFin = dataImported.dateFin.substring(0, 10)
+      this.projet.cout = dataImported.cout
+      this.projet.typeProjet = dataImported.typeProjet
+      this.projet.devise = dataImported.devise
+      this.projet.coutEstime = dataImported.coutEstime
+      this.projet.maintenance = dataImported.maintenance
+      this.projet.pays = dataImported.pays
+      this.projet.contact = dataImported.contact
+      this.projet.serviceH = dataImported.serviceH
+      this.projet.materiel = dataImported.materiel
+      this.projet.employe = dataImported.employe
+      this.projet.technologie = dataImported.technologie
+      console.log(this.projet)
+    }).catch(error => console.log(error))
+    axios.get('http://127.0.0.1:8000/api/technologies/').then(res => {
+      const dataImported = res.data['hydra:member']
+      for (const key in dataImported) {
+        const techno = dataImported[key]
+        if (techno.projet.id === this.projet.id) {
+          this.technos.push(techno)
+        }
+      }
+    }).catch(error => console.log(error))
   }
 }
 </script>
